5cd3b9ab2fdd4650b816372ddb43521b43de076e
piry9200 <pirypiry06@gmail.com>
2025-06-14 16:06:21 +0900

    fix:java.utilパッケージに含まれるクラスを，ワイルドカードを使わずにインポートするようにした

--- src/aitech/maslab/piry/fileorganizer/FileOrganizer.java
+++ src/aitech/maslab/piry/fileorganizer/FileOrganizer.java
@@ -6,7 +6,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.List;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.Scanner;
 import java.util.stream.Stream;
 
 /**
